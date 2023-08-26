681cfea3a68bd5c8c4e23ef8f01a87ebc8fab5a1
Sanyuktha Kumbla <sanyu@Sanyukthas-MacBook-Pro.local>
2023-08-26 17:24:50 -0400

    added the changes for work section related to proper name and shadow

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,14 @@
 
-const Card = ({imgUrl}) =>{
+const Card = ({imgUrl,text}) =>{
 
 
     return(
         <div className="container shadow-md h-1/5">
+            <div className="border-gray-50 p-2 border-4">
             <img src={require("./assets/"+(imgUrl))}  alt=""></img>
-            <p>Social Developer Website</p>
+            <p className="font-bold text-md">{text}</p>
+            </div>
+         
         </div>
     )
 
--- src/Work.js
+++ src/Work.js
@@ -8,12 +8,12 @@ import Card from "./Card";
         <h1  className="text-4xl text-center pt-20 ">WORK</h1>
         <div className="flex items-center justify-center pt-10">
         <div className="grid md:grid-cols-3 gap-8 w-3/5 ">
-        <Card  imgUrl={"work1.png"}/>
-        <Card  imgUrl={"work2.png"}/>
-        <Card  imgUrl={"work3.png"}/>
-        <Card  imgUrl={"work4.png"}/>
-        <Card  imgUrl={"work5.png"}/>
-        <Card  imgUrl={"work6.png"}/>
+        <Card  imgUrl={"work1.png"} text={"Ecommerce Website"}/>
+        <Card  imgUrl={"work2.png"} text={"Make your Burger "}/>
+        <Card  imgUrl={"work3.png"} text={"Search user on Github"}/>
+        <Card  imgUrl={"work4.png"} text={"Expense Tracker"}/>
+        <Card  imgUrl={"work5.png"} text={"To-do App"}/>
+        <Card  imgUrl={"work6.png"} text={"Search Images"}/>
         </div>
         </div>
     
